cb2d26b897ececc7541819a7bdab654c06a8a13f
broulston <benjamin.roulston@protonmail.com>
2019-04-23 10:44:56 -0400

    switch to pytest

--- test_SB2.py
+++ test_SB2.py
@@ -7,11 +7,10 @@ import astropy.constants as const
 import bisect
 
 import makeSB2temp
-
-SB2 = makeSB2temp.SB2("179.21537+018.78220_57502_3-112133641.txt", "G1","164.12292-002.58138_57434_3-110152864.txt", "WD1")
-SB2.plotCompositeSB2() 
-SB2.saveCompositeSB2()
-
-
-
 #"206.48379+001.07330_57057_4-21387.txt", "M0"
+
+def test_makeSB2():
+    SB2 = makeSB2temp.SB2("179.21537+018.78220_57502_3-112133641.txt", "G1","164.12292-002.58138_57434_3-110152864.txt", "WD1")
+    SB2.plotCompositeSB2() 
+    SB2.saveCompositeSB2()
+    
