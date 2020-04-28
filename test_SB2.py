4628fa822f883677297362b6d3eda2a3f26e7995
broulston <benjamin.roulston@protonmail.com>
2020-04-27 21:42:32 -0400

    Update test_SB2.py

--- test_SB2.py
+++ test_SB2.py
@@ -1,4 +1,4 @@
-import makeSB2temp as SB2
+from SB2 import SB2
 
 
 def test_makeSB2():
@@ -9,7 +9,7 @@ def test_makeSB2():
     filename1 = "C/" + spectype1 + ".txt"
     filename2 = "WD/" + spectype2 + ".txt"
 
-    this_SB2 = SB2.SB2(individualSpec_dir + filename1, spectype1,
+    this_SB2 = SB2(individualSpec_dir + filename1, spectype1,
                        individualSpec_dir + filename2, spectype2)
 
     if this_SB2.isCOMBO:
