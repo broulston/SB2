7f67eafee675b478c1482827a957eec928a57223
broulston <benjamin.roulston@protonmail.com>
2020-04-27 22:16:48 -0400

    change test for fits files

--- test_SB2.py
+++ test_SB2.py
@@ -6,8 +6,8 @@ def test_makeSB2():
     spectype1 = "C2"
     spectype2 = "WD3"
 
-    filename1 = "C/" + spectype1 + ".txt"
-    filename2 = "WD/" + spectype2 + ".txt"
+    filename1 = "C/" + spectype1 + ".fits"
+    filename2 = "WD/" + spectype2 + ".fits"
 
     this_SB2 = SB2(individualSpec_dir + filename1, spectype1,
                    individualSpec_dir + filename2, spectype2)
